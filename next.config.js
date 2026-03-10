906463ac0ff333de02aa479d21d15e137be78d24
ASP-ML <nperez@usfq.edu.ec>
2026-03-10 22:23:59 +0400

    github  pages images fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? "/mypage" : "";
+// const basePath = isProd ? "/mypage" : "";
 
 const nextConfig = {
   output: "export",
-  basePath: basePath, // Aplica /mypage solo en producción
+  basePath: isProd ? "/mypage" : "", // Aplica /mypage solo en producción
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
   env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
+    NEXT_PUBLIC_BASE_PATH: isProd ? "/mypage" : "",
   },
 };
 
